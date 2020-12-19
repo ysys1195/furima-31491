20b2a800511c323b00937d2f555758ec5126c6ab
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-19 16:48:34 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
+  validates :birthday, presence: true
   
 end
