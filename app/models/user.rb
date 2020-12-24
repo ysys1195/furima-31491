9be5c1eb52ead79ea9b074e775391e291949a237
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-24 11:52:11 +0900

    ユーザー管理機能 修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
@@ -15,5 +14,8 @@ class User < ApplicationRecord
     validates :first_name_katakana
     validates :last_name_katakana
   end
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 end
