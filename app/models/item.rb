736ecab5cc9bc11e7f6ef461be82e773e0e148be
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-26 19:16:48 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status 
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :category_id,  numericality: { other_than: 1 }
+  validates :status_id,  numericality: { other_than: 1 }
+  validates :price,  numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture,  numericality: { other_than: 1 }
+  validates :delivery_day_id,  numericality: { other_than: 1 }
 end
