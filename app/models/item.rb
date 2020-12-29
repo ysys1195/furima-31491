7ad69aeb32d1d621d78813f3c1418f26039a4efa
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-29 14:46:47 +0900

    バリデーションの設定2

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
   belongs_to :user
+  has_one_attached :image
 
   validates :name, presence: true
+  validates :detail, presence: true
   validates :category_id,  numericality: { other_than: 1 }
   validates :status_id,  numericality: { other_than: 1 }
   validates :price,  numericality: { other_than: 1 }
