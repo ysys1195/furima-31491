40e1c1fdc605b95348d042deb07faf49e954c182
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-29 15:08:01 +0900

    コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,19 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index]
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :status_id, :price, :delivery_fee_id, :prefecture, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
 end
