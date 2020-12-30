b559de781122b988f3deb84664734a9abbbfed46
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-30 15:37:35 +0900

    バリデーションの設定3

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :detail, presence: true
-  validates :category_id,  numericality: { other_than: 1 }
-  validates :status_id,  numericality: { other_than: 1 }
-  validates :price,  numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :prefecture,  numericality: { other_than: 1 }
-  validates :delivery_day_id,  numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :price, format: {with: /\d+/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+  
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 end
