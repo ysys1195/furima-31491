3b9688939cc22d3742cd1e00cc9bb61a541d1d1e
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-12-30 15:38:19 +0900

    商品出品機能ビュー2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :detail, :category_id, :status_id, :price, :delivery_fee_id, :prefecture, :delivery_day_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :detail, :category_id, :status_id, :price, :delivery_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
 end
