9b7ea53862ef289a6f4641ab1485ef4ac8defe06
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-06 23:24:20 +0900

    商品出品機能 単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {Faker::Commerce.product_name}
+    detail          {Faker::Lorem.sentence}
+    price           {Faker::Commerce.price(range: 300..9999999)}
+    category_id     {Faker::Number.within(range: 2..11)}
+    status_id       {Faker::Number.within(range: 2..7)}
+    delivery_fee_id {Faker::Number.within(range: 2..3)}
+    prefecture_id   {Faker::Number.within(range: 2..48)}
+    delivery_day_id {Faker::Number.within(range: 2..4)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "商品出品機能" do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+    context "商品出品成功" do
+      it "image/name/detail/price/category_id/status_id/delivery_fee_id/prefecture_id/delivery_day_idが存在すれば保存できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context "商品出品失敗" do
+      it "imageが存在しなければ保存できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "nameが存在しなければ保存できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "detailが存在しなければ保存できない" do
+        @item.detail = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+      it "priceが存在しなければ保存できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "priceが¥300未満だと保存できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "priceが¥9,999,999より大きいと保存できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "priceが全角数字だと保存できない" do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "category_idが存在しなければ保存できない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it "status_idが存在しなければ保存できない" do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+      it "delivery_fee_idが存在しなければ保存できない" do
+        @item.delivery_fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank", "Delivery fee is not a number")
+      end
+      it "prefecture_idが存在しなければ保存できない" do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+      end
+      it "delivery_day_idが存在しなければ保存できない" do
+        @item.delivery_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank", "Delivery day is not a number")
+      end
+      it "userが紐づいていないと保存できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
