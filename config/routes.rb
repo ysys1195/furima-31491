36c3c7c3582b882f13141fc01fb19ff8b4e8aa89
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-16 19:10:33 +0900

    商品購入機能 事前準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'purchases/new'
+  get 'purchases/create'
   devise_for :users
   root to: "items#index"
   resources :items, except: [:index]
+  resources :purchases, only: [:new, :create]
 end
