5560a20b4b889386201d9a6a2774a121c58b0a7e
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-16 14:05:50 +0900

    商品削除機能 修正点1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item_id, only: [:show, :edit, :update]
+  before_action :find_item_id, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -37,8 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
