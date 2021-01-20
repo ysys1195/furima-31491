f61f58808e9fa78a385afbe4f0e6ee38d383b725
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-20 23:15:15 +0900

    商品購入機能 モデル

---
+++ db/migrate/20200119135842_create_purchase_logs.rb
@@ -0,0 +1,9 @@
+class CreatePurchaseLogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchase_logs do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
