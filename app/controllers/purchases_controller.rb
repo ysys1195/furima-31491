9ffdf7df79205b3e1f04edfecb1e79ccedcc49c1
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-01-20 23:14:22 +0900

    商品購入機能 コントローラー

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,23 @@
 class PurchasesController < ApplicationController
   def index
+    @purchase_item = PurchaseItem.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase_item = PurchaseItem.new(purchase_item_params)
+    if @purchase_item.valid?
+      @purchase_item.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_item_params
+    params.require(:purchase_item).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_log_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
